e53932e833a8ecbf218e2a330585cf600ee8a91e
HuyTran <trankhachuy94@gmail.com>
2020-11-26 08:43:34 +0700

    add react app

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./components/navBar";
+import React from "react";
+
+function App() {
+  return (
+    <React.Fragment>
+      <NavBar/>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ myapp/src/components/navBar.js
@@ -0,0 +1,11 @@
+function NavBar () {
+    return (
+        <div className="nav-bar">
+            <div className="menu-icon">
+                <div className="burger"></div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
