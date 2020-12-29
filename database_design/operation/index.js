20b0c1d0c473735b6edb5641b041daa63fe321c5
HuyTran <trankhachuy94@gmail.com>
2020-12-29 08:39:09 +0700

    abc

--- database_design/operation/index.js
+++ database_design/operation/index.js
@@ -1,10 +1,26 @@
 const mysqlutil = require("./utils/mysql");
 const fs = require("fs-extra");
 
+var executes = [
+    'data_product_eav.txt',
+    'data_product_eav_datetime.txt',
+    'data_product_eav_decimal.txt',
+    'data_product_eav_int.txt',
+    'data_product_eav_multi_value.txt',
+    'data_product_eav_text.txt',
+    'data_product_eav_varchar.txt',
+    'data_product_entity.txt',
+    'ecommerce_tables.csv',
+    'operate_1.txt',
+    'schema_init.txt',
+    'sql_note.txt',
+    'test.html'
+];
+
 (async () => {
-    let dir = await fs.readdir("./");
+    let dir = await fs.readdir("../");
     for(let i = 0; i < dir.length; i++){
-        let stat = await fs.stat("./" + dir[i]);
+        let stat = await fs.stat("../" + dir[i]);
         if(!stat.isFile()){
             dir.splice(i, 1);
             i -= 1;
